579f953e47aab24fb673d4307b739974032a133f
Ашутов Алексей <ashutov@iteco-inno.ru>
2023-02-09 14:16:07 +0300

    -init

--- src/main/java/shadow/dev/spring/ApplicationRunner.java
+++ src/main/java/shadow/dev/spring/ApplicationRunner.java
@@ -1,5 +1,6 @@
 package shadow.dev.spring;
 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import shadow.dev.spring.datatabase.pool.ConnectionPool;
 import shadow.dev.spring.datatabase.repository.CompanyRepository;
 import shadow.dev.spring.datatabase.repository.UserRepository;
@@ -9,18 +10,9 @@ import shadow.dev.spring.service.UserService;
 public class ApplicationRunner {
 
     public static void main(String[] args) {
-
-        var container = new Container();
-//        var connectionPool = new ConnectionPool();
-//        var userRepository = new UserRepository(connectionPool);
-//        var companyRepository = new CompanyRepository(connectionPool);
-//        var userService = new UserService(userRepository, companyRepository);
-
-//        var connectionPool = container.get(ConnectionPool.class);
-//        var userRepository = container.get(UserRepository.class);
-//        var companyRepository = container.get(CompanyRepository.class);
-
-        var userService = container.get(UserService.class);
+        var context = new ClassPathXmlApplicationContext("application.xml");
+        var connectionPool = context.getBean("pool2", ConnectionPool.class);
+        System.out.println(connectionPool);
     }
 
 }
