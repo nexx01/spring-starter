0f7757887d96d9e202c59f2fecbe18644c7e7e64
Ашутов Алексей <ashutov@iteco-inno.ru>
2023-02-10 14:30:25 +0300

    -init

--- src/main/java/shadow/dev/spring/ApplicationRunner.java
+++ src/main/java/shadow/dev/spring/ApplicationRunner.java
@@ -1,5 +1,6 @@
 package shadow.dev.spring;
 
+import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import shadow.dev.spring.datatabase.pool.ConnectionPool;
 import shadow.dev.spring.datatabase.repository.CompanyRepository;
@@ -7,9 +8,17 @@ import shadow.dev.spring.datatabase.repository.UserRepository;
 import shadow.dev.spring.ioc.Container;
 import shadow.dev.spring.service.UserService;
 
+import java.io.Serializable;
+import java.lang.invoke.SerializedLambda;
+
 public class ApplicationRunner {
 
     public static void main(String[] args) {
+        var value = "hello";
+        System.out.println(CharSequence.class.isAssignableFrom(value.getClass()));
+        System.out.println(BeanFactoryPostProcessor.class.isAssignableFrom(value.getClass()));
+        System.out.println(Serializable.class.isAssignableFrom(value.getClass()));
+
         try (var context = new ClassPathXmlApplicationContext("application.xml")) {
             var connectionPool = context.getBean("pool1", ConnectionPool.class);
             System.out.println(connectionPool);
