8e08204255205b5e89dbe206cb2a8d5e0b252725
Ашутов Алексей <ashutov@iteco-inno.ru>
2023-02-10 12:16:32 +0300

    -init

--- src/main/java/shadow/dev/spring/ApplicationRunner.java
+++ src/main/java/shadow/dev/spring/ApplicationRunner.java
@@ -10,11 +10,12 @@ import shadow.dev.spring.service.UserService;
 public class ApplicationRunner {
 
     public static void main(String[] args) {
-        var context = new ClassPathXmlApplicationContext("application.xml");
-        var connectionPool = context.getBean("pool1", ConnectionPool.class);
-        System.out.println(connectionPool);
-        var companyRepository = context.getBean("companyRepository", CompanyRepository.class);
-        System.out.println(companyRepository);
+        try (var context = new ClassPathXmlApplicationContext("application.xml")) {
+            var connectionPool = context.getBean("pool1", ConnectionPool.class);
+            System.out.println(connectionPool);
+            var companyRepository = context.getBean("companyRepository", CompanyRepository.class);
+            System.out.println(companyRepository);
+        }
     }
 
 }
