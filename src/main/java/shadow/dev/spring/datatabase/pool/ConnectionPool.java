1847e11a939c869933f0563733c8aaa3d17c18f6
Ашутов Алексей <ashutov@iteco-inno.ru>
2023-02-12 15:39:20 +0300

    -

--- src/main/java/shadow/dev/spring/datatabase/pool/ConnectionPool.java
+++ src/main/java/shadow/dev/spring/datatabase/pool/ConnectionPool.java
@@ -11,12 +11,15 @@ import java.util.Map;
 //public class ConnectionPool {
 public class ConnectionPool implements InitializingBean, DisposableBean {
 
-    private final String username;
-    private final Integer poolSize;
+    private  String username;
+    private  Integer poolSize;
 
-    private final List<Object> args;
+    private  List<Object> args;
 
     private Map<String, Object> properties;
+
+    public ConnectionPool() {
+    }
 
     public ConnectionPool(String username, Integer poolSize, List<Object> args, Map<String, Object> properties) {
         this.username = username;
--- src/main/java/shadow/dev/spring/datatabase/repository/CompanyRepository.java
+++ src/main/java/shadow/dev/spring/datatabase/repository/CompanyRepository.java
@@ -1,5 +1,8 @@
 package shadow.dev.spring.datatabase.repository;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import shadow.dev.spring.bpp.Auditing;
 import shadow.dev.spring.bpp.InjectBean;
 import shadow.dev.spring.bpp.Transaction;
@@ -7,13 +10,28 @@ import shadow.dev.spring.datatabase.entity.Company;
 import shadow.dev.spring.datatabase.pool.ConnectionPool;
 
 import javax.annotation.PostConstruct;
+import javax.annotation.Resource;
+import java.util.List;
 import java.util.Optional;
 
 @Auditing
 @Transaction
 public class CompanyRepository implements CrudRepository<Integer, Company> {
-    @InjectBean
-    private ConnectionPool connectionPool;
+
+    @Autowired
+//    public void setPool1(ConnectionPool pool1) {
+//        this.pool1 = pool1;
+//    }
+
+    @Value("${db.pool.size}")
+    private Integer poolSize;
+
+    //    @Autowired
+//    @Qualifier("pool1")
+//    @Resource(name = "pool2")
+    private ConnectionPool pool1;
+    @Autowired
+    private List<ConnectionPool> connectionPools;
 
     @PostConstruct
     private void init() {
