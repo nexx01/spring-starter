7908a69b450855c841411385c1a4d1c71404008c
Ашутов Алексей <ashutov@iteco-inno.ru>
2023-02-13 12:39:54 +0300

    -

--- src/main/java/shadow/dev/spring/config/ApplicationConfiguration.java
+++ src/main/java/shadow/dev/spring/config/ApplicationConfiguration.java
@@ -1,12 +1,16 @@
 package shadow.dev.spring.config;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.*;
 import org.springframework.context.annotation.ComponentScan.Filter;
 import org.springframework.stereotype.Component;
+import shadow.dev.spring.datatabase.pool.ConnectionPool;
 import shadow.dev.spring.datatabase.repository.CrudRepository;
+import shadow.dev.spring.datatabase.repository.UserRepository;
 import shadow.dev.web.ConfigWebConfiguration;
 
-@Configuration
+@Configuration(proxyBeanMethods = true)
 @PropertySource("classpath:application.properties")
 @ComponentScan(basePackages = "shadow.dev.spring",
         useDefaultFilters = false,
@@ -18,4 +22,28 @@ import shadow.dev.web.ConfigWebConfiguration;
 //@ImportResource("classpath:application.xml")
 @Import(ConfigWebConfiguration.class)
 public class ApplicationConfiguration {
+    @Bean
+    @Scope(BeanDefinition.SCOPE_SINGLETON)
+    public ConnectionPool pool2(@Value("${db.username}") String username) {
+        return new ConnectionPool(username, 20);
+    }
+
+
+    @Bean
+    @Scope(BeanDefinition.SCOPE_SINGLETON)
+    public ConnectionPool pool3() {
+        return new ConnectionPool("test-pool", 20);
+    }
+
+    @Bean
+    public UserRepository userRepository2(ConnectionPool pool2) {
+        return new UserRepository(pool2);
+    }
+
+    @Bean
+    public UserRepository userRepository3() {
+        ConnectionPool connectionPool1 = pool3();
+        ConnectionPool connectionPool2 = pool3();
+        return new UserRepository(pool3());
+    }
 }
--- src/main/java/shadow/dev/spring/datatabase/repository/UserRepository.java
+++ src/main/java/shadow/dev/spring/datatabase/repository/UserRepository.java
@@ -1,5 +1,6 @@
 package shadow.dev.spring.datatabase.repository;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Repository;
@@ -9,9 +10,10 @@ import shadow.dev.spring.datatabase.pool.ConnectionPool;
 @Scope(BeanDefinition.SCOPE_PROTOTYPE)
 public class UserRepository {
 
+
     private final ConnectionPool connectionPool;
 
-    public UserRepository(ConnectionPool connectionPool) {
+    public UserRepository(@Qualifier("pool2") ConnectionPool connectionPool) {
         this.connectionPool = connectionPool;
     }
 
